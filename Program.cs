2fd9627d02361fe1b6d5f90db329fa7042576682
Karel Škrabal <k.skrabal@seznam.cz>
2023-11-22 12:49:44 +0100

    input arguments (T#1)

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace PingTester
+﻿using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+
+namespace PingTester
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			#region T#1 input args
+			//TODO- 0#1 additional options for output, serialization, (factory????)
+			if (args.Length == 0)
+				throw new ArgumentNullException("Wrong arguments");
+
+			if (args.Length < 2)
+				throw new ArgumentOutOfRangeException("No ip address");
+
+			if (!int.TryParse(args[0], out int number))
+				throw new ArgumentNullException("Wrong argument for testing time period");
+
+			var setting = new Setting(number, args.Skip(1));
+			#endregion
+
+#if DEBUG
+			Console.WriteLine($"TimePeriod={setting.TimePeriod}");
+			Console.WriteLine($"Ip addresses: ");
+			foreach (var ip in setting.Ips)
+				Console.WriteLine($"{ip}");
+#endif
 		}
 	}
 }
---
+++ Setting.cs
@@ -0,0 +1,14 @@
+﻿namespace PingTester
+{
+	public class Setting
+	{
+		public int TimePeriod { get; }
+		public IEnumerable<string> Ips { get; }
+
+		public Setting(int timePeriod, IEnumerable<string> ips)
+		{
+			TimePeriod = timePeriod;
+			Ips = ips;
+		}
+	}
+}
